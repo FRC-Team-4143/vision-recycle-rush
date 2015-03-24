2075bd40f7e65072ef3e1361d46bad551bd1bcd3
Ryan Shoff <ryanshoff@gmail.com>
2015-03-24 03:31:57 +0000

    after CIR

--- filters.py
+++ filters.py
@@ -1,6 +1,6 @@
 low_h = 28
-high_h = 66
+high_h = 110
 low_s = 0
-high_s = 23
+high_s = 255
 low_v = 230 
 high_v = 255
--- start.sh
+++ start.sh
@@ -4,15 +4,15 @@ echo -1 > /sys/module/usbcore/parameters/autosuspend
 echo -1 > /sys/kernel/debug/tegra_hdmi/hotplug
 echo 4 > /sys/class/graphics/fb0/blank
 
-uvcdynctrl --set='Exposure, Auto' 1 
+#uvcdynctrl --set='Exposure, Auto' 1 
 
 #uvcdynctrl --set='Exposure (Absolute)' 5 # Min
 #uvcdynctrl --set='Exposure (Absolute)' 20000 # Max
-uvcdynctrl --set='Exposure (Absolute)' 10
+#uvcdynctrl --set='Exposure (Absolute)' 10
 
-#uvcdynctrl --set='Brightness' 30   # Min
+#uvcdynctrl --set='Brightness' 5  # Min
 #uvcdynctrl --set='Brightness' 255   # Max
-uvcdynctrl --set='Brightness' 30
+uvcdynctrl --set='Brightness' 5
 
 cd /home/ubuntu/vision-recycle-rush
 echo "Vision program starting..."
